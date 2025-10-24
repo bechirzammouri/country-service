959712bf7bc21aa27391b3b2d784e471141773f3
Bechir Zammouri <bechir.zammouri@ensi-uma.tn>
2025-10-24 19:44:36 +0000

    cleaning repo

--- src/main/java/com/countryservice/demo/repositories/CountryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.countryservice.demo.repositories;
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.countryservice.demo.beans.Country;
-
-public interface CountryRepository extends JpaRepository<Country,Integer> {
-
-}
