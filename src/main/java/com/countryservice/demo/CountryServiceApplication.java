92d1f61cb30568b71b3df1fc650802db1c79070d
Bechir Zammouri <bechir.zammouri@ensi-uma.tn>
2025-10-25 19:00:32 +0000

    Fix WAR deployment: add SpringBootServletInitializer and mark Tomcat as provided

--- src/main/java/com/countryservice/demo/CountryServiceApplication.java
+++ src/main/java/com/countryservice/demo/CountryServiceApplication.java
@@ -2,9 +2,16 @@ package com.countryservice.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CountryServiceApplication {
+public class CountryServiceApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(CountryServiceApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(CountryServiceApplication.class, args);
